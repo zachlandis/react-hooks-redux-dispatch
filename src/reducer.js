978d42462fc162d645e7a5068b69605b0e252f99
Zach <zachlandis91@gmail.com>
2023-09-11 11:33:02 -0600

    commit tree fbfb800a8646228bfcf4b2b8ecb4b3ac1892c406

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +9,14 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+render();
